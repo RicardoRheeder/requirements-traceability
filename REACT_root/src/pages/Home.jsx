85810e2247db7dd4d13ff10bce5ca350c3f9174c
Ricardo Rheeder <rir954@usask.ca>
2020-10-04 13:17:48 -0600

    Prototyped - not working on boilerplate, as it has many dependencies. Not a great experience.

--- REACT_root/src/pages/Home.jsx
+++ REACT_root/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
+import { DigraphPrototype } from './'
+
 export default class Home extends Component {
     render() {
         return (
             <div>
                 HOME
+                <DigraphPrototype />
             </div>
         )
     }
--- REACT_root/src/pages/index.jsx
+++ REACT_root/src/pages/index.jsx
@@ -3,3 +3,6 @@ export { default as ReactPage } from './ReactPage'; // KEPT AS AN EXAMPLE OF USI
 export { default as Home } from './Home';
 export { default as NotFound } from './NotFound';
 export { default as LogIn } from './LogIn';
+
+// Digraph prototype
+export { default as DigraphPrototype } from './DigraphPrototype';
