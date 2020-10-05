f6ae317661292c6b9ec828ed6bf2546faa48a0f5
Lat740 <levithompson17@gmail.com>
2020-10-05 12:59:15 -0600

    Reformat smoke tests with prettier

--- REACT_root/src/pages/smoke.test.js
+++ REACT_root/src/pages/smoke.test.js
@@ -10,7 +10,6 @@ const DOCUMENT_NAME = "Example Document";
 const NODE_NAME = "Example Requirement";
 const NODE_DESCRIPTION = "Example description for new requirement";
 
-
 //Login page values
 const LOGIN_PAGE_PATH = "Login.jsx";
 const LOGIN_USERFIELD_ID = "username-field";
@@ -26,13 +25,11 @@ const NEW_DOCUMENT_CONFIRM_BUTTON_ID = "new-document-name";
 const SPLITPANE_NODES_ID = "node-pane";
 const SPLITPANE_NEW_NODE_NAMEFIELD_ID = "node-name-field";
 const SPLITPANE_NEW_NODE_DESCFIELD_ID = "node-description-field";
+const SPLITPANE_NEW_NODE_CONFIRM_BUTTON_ID = "node-confirm--button";
 
-
-
-const {Builder, By, Key, util} = require("selenium-webdriver");
+const { Builder, By, Key, util } = require("selenium-webdriver");
 async function smokeTest() {
-    try {
-
+  try {
     //Initiate driver for desired browser
     let driver = await new Builder().forBrowser(BROWSER).build();
 
@@ -40,22 +37,35 @@ async function smokeTest() {
     await driver.get(LOGIN_PAGE_PATH);
     await driver.findElement(By.id(LOGIN_USERFIELD_ID)).sendKeys(USERNAME);
     await driver.findElement(By.id(LOGIN_PASSFIELD_ID)).sendKeys(PASSWORD);
+    await driver.takeSnapshot();
     await driver.findElement(By.id(LOGIN_BUTTON_ID)).click();
 
     //Create a new requirements document
     await driver.findElement(By.id(NEW_DOCUMENT_BUTTON_ID)).click();
-    await driver.findElement(By.id(NEW_DOCUMENT_NAMEFIELD_ID)).sendKeys(DOCUMENT_NAME);
+    await driver
+      .findElement(By.id(NEW_DOCUMENT_NAMEFIELD_ID))
+      .sendKeys(DOCUMENT_NAME);
+    await driver.takeSnapshot();
     await driver.findElement(By.id(NEW_DOCUMENT_CONFIRM_BUTTON_ID)).click();
 
     //Create a new requirement node in splitpane
     await driver.findElement(By.id(SPLITPANE_NODES_ID)).click();
-    await driver.findElement(By.id(SPLITPANE_NEW_NODE_NAMEFIELD_ID)).sendKeys(NODE_NAME);
-    await driver.findElement(By.id(SPLITPANE_NEW_NODE_DESCFIELD_ID)).sendKeys(NODE_DESCRIPTION);
-    }
-    
-    catch(e) {
-        webdriverErrorHandler(e,driver)
-    }
+    await driver
+      .findElement(By.id(SPLITPANE_NEW_NODE_NAMEFIELD_ID))
+      .sendKeys(NODE_NAME);
+    await driver
+      .findElement(By.id(SPLITPANE_NEW_NODE_DESCFIELD_ID))
+      .sendKeys(NODE_DESCRIPTION);
+    await driver.takeSnapshot();
+    await driver
+      .findElement(By.id(SPLITPANE_NEW_NODE_CONFIRM_BUTTON_ID))
+      .click();
+  } catch (e) {
+    webdriverErrorHandler(e, driver);
+  }
+}
 
+function webdriverErrorHandler(err, driver) {
+  console.error("Unhandled exception: " + err.message);
 }
 smokeTest();
