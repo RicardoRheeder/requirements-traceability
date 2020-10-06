571fead6b5ea43c47d3c589a1c0a93a6b983a54d
jrk057 <jrk057@usask.ca>
2020-10-06 15:48:04 -0600

    setup for transform docs

--- client/src/components/ReactComponent.test.jsx
+++ client/src/components/ReactComponent.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import app from './app.jsx';    
+import ReactComponent from './ReactComponent.jsx';    
 
 import Enzyme, { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json'; 
@@ -8,10 +8,10 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-describe('<app />', () => {
+describe('<ReactComponent />', () => {
     it('renders the test component', () => {
-        const testcomp = shallow(<app />);
-        expect(testcomp.type).toEqual(app);
+        const testcomp = shallow(<ReactComponent />);
+        expect(testcomp.type).toEqual(Adapter);
     });
 
 });
