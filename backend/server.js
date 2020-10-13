d61e6c01cb504c4790f6575275735d4d48385f3e
Faiz <faizqureshi111@gmail.com>
2020-10-13 12:33:01 -0600

    added connection to mongodb atlas as a temp database until we get our VM up.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 // requiring dotenv for env
 const dotenv = require("dotenv");
@@ -11,6 +12,16 @@ const PORT = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// connect to mongoDB atlas
+const uri = process.env.ATLAS_URI;
+console.log(uri);
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection established successfully");
+});
 
 app.get("/", (req, res) => {
   res.send("Node.js & Express are working. ");
