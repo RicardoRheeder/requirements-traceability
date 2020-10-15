57ca0128696abbbcc32f234809ba44468fef641a
Faiz <faizqureshi111@gmail.com>
2020-10-15 17:38:31 -0600

    got .env file working and am now using it in app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -26,8 +26,6 @@ class App extends Component {
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-console.log(process.env);
-// console.log(domain);
 
 ReactDOM.render(
   <Auth0Provider
