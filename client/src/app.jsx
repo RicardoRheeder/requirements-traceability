19a0f165296f1c52078f87dba107f4fe2c4ee1e5
Faiz <faizqureshi111@gmail.com>
2020-10-15 12:03:59 -0600

    set up auth0 sdk in app.jsx file

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,25 +1,39 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+// auth0 SDK
+import { Auth0Provider } from "@auth0/auth0-react";
 
 // Root Stylesheet
-import './sass/main';
-import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
+import "./sass/main";
+import "react-sortable-tree/style.css"; // This only needs to be imported once in your app
 
-import Router from './Router';
+import Router from "./Router";
 
 // Redux Config
-import { configureStore } from './redux';
-import { Provider } from 'react-redux';
+import { configureStore } from "./redux";
+import { Provider } from "react-redux";
 const store = configureStore();
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router/>
+        <Router />
       </Provider>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+ReactDOM.render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);
