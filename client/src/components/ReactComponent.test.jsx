122c5264799859560f7ab57982aed869f58b8e17
jrk057 <jrk057@usask.ca>
2020-10-18 01:48:24 -0600

    adding full support for Integration testing and a more fleshed out unit test

--- client/src/components/ReactComponent.test.jsx
+++ client/src/components/ReactComponent.test.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {add} from './app.jsx';    
+import ReactComponent from './ReactComponent.jsx';    
 
-import Enzyme, { shallow, mount } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json'; 
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-console.log(add(1,3))
-
-test('add function', () =>{
-    const val = add(1,2)
-    expect(val).toBe(3)
-});
+test('test component renders', () =>{
+    shallow(<ReactComponent/>)
+})
