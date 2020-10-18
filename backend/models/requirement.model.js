15cba97a0904ce953bf015f039a985ab6c2f1b63
Mitchell Yellowlees (miy456) <miy456@mail.usask.ca>
2020-10-18 16:47:38 -0600

    Upload requirement model

---
+++ backend/models/requirement.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const requirement = require("../models/requirement.model");
+
+const requirementSchema = new Schema(
+  {
+    name: {type: String, required: true, unique: true}, 
+    creator: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    body: {type: Text},
+    currentVersion: {type: String, required: true},
+    isDeleted: {type: Boolean, required: true},
+    versions: [{ type: Schema.Types.ObjectId, ref: "Requirement"}],
+    dependencies: [{ type: Schema.Types.ObjectId, ref: "Requirement"}],
+    createdTime: {type: Date, required: true},
+    isBeingEdited: {type: Boolean, required: true},
+    isChanged: {type: Boolean, required: true},
+  },
+  {
+        timestamps: true,
+  }
+);
+
+const Requirement = mongoose.model("Requirement", requirementSchema);
+
+module.exports = Requirement;
