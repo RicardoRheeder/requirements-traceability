d70b1d8e67bb2679222f57a2566038c679db5a7e
Mitchell Yellowlees (miy456) <miy456@mail.usask.ca>
2020-10-19 15:28:43 -0600

    Added comments outlining required features

---
+++ backend/routes/Requirement.js
@@ -0,0 +1,14 @@
+
+// Create a new Requirement
+
+// Update a Requirement
+
+// Get all Requirements
+
+// Get a specific Requirement
+
+// Get all versions of a Requirement (and populate)
+
+// Delete a Requirement
+
+// Delete all Requirements
