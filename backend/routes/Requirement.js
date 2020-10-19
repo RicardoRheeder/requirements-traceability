d6929615784ceb5db5ff4b670c4513e07665cff9
Mitchell Yellowlees (miy456) <miy456@mail.usask.ca>
2020-10-19 17:31:30 -0600

    Added base for route for creating a new requirement

--- backend/routes/Requirement.js
+++ backend/routes/Requirement.js
@@ -1,5 +1,26 @@
+const router = require('express').Router();
+const Requirement = require('../models/requirement.model');
 
 // Create a new Requirement
+router.route("/create-requirement").post((req, res)=>{
+
+    const title = req.body.title;
+    const user = req.body.user;
+
+    // TO DO Check syntax
+    const newRequirement = new Requirement({
+        name: title,
+        creator: user,
+        currentVersion: '1',
+        isDeleted: false,
+        isBeingEdited: false,   // TO DO Double check if creation automatically begins editing
+        isChanged: false
+    })
+
+    newRequirement.save().then((newRequirement)=>{
+        res.json("Requirement saved to db " + newRequirement)
+    }).catch((err)=>res.status(400).json("Error occured: Coudl not save "+ err))
+});
 
 // Update a Requirement
 
