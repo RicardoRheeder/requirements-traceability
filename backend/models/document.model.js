f65f928b1f619b3534688d8d06f778b897081e5a
Mitchell Yellowlees (miy456) <miy456@mail.usask.ca>
2020-10-18 18:05:07 -0600

    Remove redundant date field

--- backend/models/document.model.js
+++ backend/models/document.model.js
@@ -8,7 +8,6 @@ const documentSchema = new Schema(
     title: {type: String, required: true}, 
     admin: {type: Schema.Types.ObjectId, ref: "User"},
     collaborators: [{type: Schema.Types.ObjectId, ref: "User"}],
-    createdDate: {type: Date, required: true},
     requirements: [{ type: Schema.Types.ObjectId, ref: "Requirement"}],
     version: [{ type: Schema.Types.ObjectId, ref: "Document"}],
   },
