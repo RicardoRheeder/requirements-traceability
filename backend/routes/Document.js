c9d745bbf797c5ef089536f30c49a803e4ed9f37
Faiz <faizqureshi111@gmail.com>
2020-10-19 17:33:33 -0600

    cleaned up some code and added functionality to add users to document with user id, delete all users (for testing) and get all documents

--- backend/routes/Document.js
+++ backend/routes/Document.js
@@ -1,52 +1,83 @@
-const router = require('express').Router();
-const Document = require('../models/document.model');
+const router = require("express").Router();
+const User = require("../models/user.model");
+const Document = require("../models/document.model");
 
 // Post Routes*****************************************
 
 // Create a new document
-router.route("/create-document").post((req, res)=>{
+router.route("/create-document").post((req, res) => {
+  const title = req.body.title;
+  const admin = req.body.admin;
 
-    const title = req.body.title;
-    const user = req.body.user;
+  const newDocument = new Document({
+    title,
+    admin,
+  });
 
-    const newDocument = new Document({
-        title,
-        admin: user
+  newDocument
+    .save()
+    .then((newDocument) => {
+      res.json("Document saved to db " + newDocument);
     })
-
-    newDocument.save().then((newDocument)=>{
-        res.json("Document saved to db " + newDocument)
-        
-    }).catch((err)=>res.status(400).json("Error occured: Could not save "+ err))
+    .catch((err) =>
+      res.status(400).json("Error occured: Could not save " + err)
+    );
 });
 
 // Get Routes********************************************
 
+// Get all document
+router.route("/").get((req, res) => {
+  Document.find()
+    .then((docs) => res.json(docs))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
 // Get a document
-router.route("/get:id").get((req, res)=>{
+router.route("/get/:id").get((req, res) => {
+  const id = req.params.id;
 
-    const id = req.params.id;
-
-    Document.findById(id).then((doc)=> res.json(doc)).catch((err)=>{
-        res.status(400).json("Error: could not find Document with "+id);
-    })
+  Document.findById(id)
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      res.status(400).json("Error: could not find Document with " + id);
+    });
 });
 
 // Get all requirements given an id
-router.route("/get-requirements/:id").get((req, res)=>{
+router.route("/get-requirements/:id").get((req, res) => {
+  const id = req.params.id;
 
-     const id = req.params.id;
+  Document.findById(id)
+    .populate("requirements")
+    .exec()
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      res.status(400).json("Error: could not find requirements given " + id);
+    });
+});
 
-     Document.findById(id).populate("requirements").exec().then((doc)=>{
-         res.json(doc);                  
-     }).catch((err)=>{
-         res.status(400).json("Error: could not find requirements given "+ id);
-     })
-})
+// Update Routes*****************************************
 
-// Get all users
+// adding a user to a document
+router.route("/add-user/:id").patch((req, res) => {
+  Document.findByIdAndUpdate(
+    { _id: req.params.id },
+    { $addToSet: { collaborators: req.body.userId } }
+  )
+    .then((doc) => res.json(doc))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
+// Delete Routes*****************************************
 
+// delete all docs (for testing)
+router.route("/deleteAll").delete((req, res) => {
+  Document.deleteMany({})
+    .then(() => res.json("All docs deleted"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 
-
-
+module.exports = router;
