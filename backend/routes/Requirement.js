a0e5985031d9b4cab0e2f815140319b838fa16be
Mitchell Yellowlees (miy456) <miy456@mail.usask.ca>
2020-10-19 18:03:01 -0600

    Added base for route for getting a specific requirement

--- backend/routes/Requirement.js
+++ backend/routes/Requirement.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Requirement = require('../models/requirement.model');
 
+
 // Create a new Requirement
 router.route("/create-requirement").post((req, res)=>{
 
@@ -22,14 +23,28 @@ router.route("/create-requirement").post((req, res)=>{
     }).catch((err)=>res.status(400).json("Error occured: Could not save "+ err))
 });
 
+
 // TO DO: Update a Requirement
+
 
 // TO DO: Get all Requirements
 
+
 // TO DO: Get a specific Requirement
+router.route("/get:id").get((req,res)=>{
+
+    const id = req.params.id;
+
+    Requirement.findById(id).then((doc)=> res.json(doc)).catch((err)=>{
+        res.status(400).json("Error occured: could not find Requirement with "+ id);
+    })
+});
+
 
 // TO DO: Get all versions of a Requirement (and populate)
 
+
 // TO DO: Delete a Requirement
+
 
 // TO DO: Delete all Requirements
