bf3cbcb0fc390a95c4d09b58ef7d2d17b2d22e4f
Ricardo Rheeder <rir954@usask.ca>
2020-10-20 14:06:02 -0600

    Added basic styling and div structure for Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export default function Home() {
-  return <div className="home-root">HOME</div>
+  return (
+    <div className="home-root">
+      <div className="left-container">
+        <div className="home-header">Doc Tracer Logo</div>
+        <div className="home-subheader">Recent Documents</div>
+      </div>
+      <div className="right-container">RIGHT</div>
+    </div>
+  )
 }
