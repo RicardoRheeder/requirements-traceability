0ee37f10f82ba0bdbaccff4cc3946a4f5f5165dc
Ricardo Rheeder <rir954@usask.ca>
2020-10-19 23:01:11 -0600

    Fixed styling on navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,47 +1,56 @@
-import React from "react";
-import { NavLink, withRouter } from "react-router-dom";
-import { LogoutButton } from "./";
-//import * from ""
+import React, { useEffect } from 'react'
+import { NavLink, withRouter } from 'react-router-dom'
+import { LogoutButton } from './'
 
 export default function NavBar() {
+  useEffect(() => {
+    const script = document.createElement('script')
+
+    script.src = 'assets/js/agency.js'
+    script.async = true
+
+    document.body.appendChild(script)
+  }, [])
+
   return (
-    // <nav className="root-container">
-    //   <span className="root-container-span root-container-home">
-    //     <NavLink to={"/"} id="NavToHome">
-    //       <span className="navbar-home text-title">AppName</span>
-    //     </NavLink>
-    //   </span>
-
-    //   <span className="root-container-span root-container-project">
-    //     <NavLink to={"/editor"} id="NavToEditor">
-    //       <span className="navbar-project text-project">Editor</span>
-    //     </NavLink>
-    //   </span>
-
-    //   <span>
-    //     <LogoutButton />
-    //   </span>
-    // </nav>
-
-<nav className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark mainNav" id="mainNav" style={{minHeight: '100px',border: '1px solid #f96302' }}>
+    <div className="navbar-root">
+      <nav
+        className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark mainNav"
+        id="mainNav"
+      >
         <div className="container">
-          <a className="navbar-brand" href="#page-top" style={{borderColor:'#2e2e2e',color: '#f96302'}}>
-            {'{Doc Tracer Icon}'}
-            </a>
-            <button data-toggle="collapse" data-target="#navbarResponsive" className="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse"
-                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                  <i className="fa fa-bars"></i>
-                  </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="nav navbar-nav ml-auto text-uppercase">
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">Home</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#team">Editor</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#contact">About</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">Team</a></li>
-                    <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">Contact</a></li>
-                </ul>
-            </div>
+          <a className="navbar-brand">{'{Doc Tracer Icon}'}</a>
+          <div>
+            <ul className="navbar-nav text-uppercase">
+              <li className="nav-item">
+                <NavLink to={'/'} id="NavToHome">
+                  <a className="nav-link">Home</a>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/editor'} id="NavToEditor">
+                  <a className="nav-link">Editor</a>
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Team</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">Contact</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link">
+                  <LogoutButton />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    </nav>
-  );
+      </nav>
+    </div>
+  )
 }
