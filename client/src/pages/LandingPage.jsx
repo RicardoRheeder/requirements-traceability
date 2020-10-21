699c8cf1bded03e1377a4493e1c8fab32dd6b599
Ricardo Rheeder <rir954@usask.ca>
2020-10-20 20:14:20 -0600

    Updated some styling for the demo

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -4,10 +4,6 @@ import { LoginButton } from "../components";
 export default function LandingPage() {
   return (
     <div className="landing-page-root"> 
-    <div className="logo-area">
-      Doc Tracer logo here!
-      <div>Let's get started!</div>
-    </div>
       <LoginButton />
     </div>
   );
