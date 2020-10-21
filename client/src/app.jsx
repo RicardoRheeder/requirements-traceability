fdd7f13513b0e0b60b1aa06af2d1edbb3ffbfab9
jrk057 <jrk057@usask.ca>
2020-10-20 20:27:33 -0600

    fixed node_modules error

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -2,20 +2,9 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
 // Root Stylesheet
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> a2e2d6d3dee3f5a1b3ed14f636dc6873b49eb04a
 import './sass/main.scss'
 import 'react-sortable-tree/style.css' // This only needs to be imported once in your app
-=======
-import './sass/main.scss';
->>>>>>> added .test for app, app currently fails .test
-=======
-import './sass/main.scss'
-import 'react-sortable-tree/style.css' // This only needs to be imported once in your app
->>>>>>> adding snapshot testing framework and early integration testing
+
 
 import Router from './Router'
 
