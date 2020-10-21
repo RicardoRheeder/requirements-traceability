4125911f9a3a18c008915bb5b38a5b82c0179a34
Ricardo Rheeder <rir954@usask.ca>
2020-10-20 20:25:12 -0600

    Added files that were accidentally removed

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,10 +1,15 @@
-import React from "react";
-import { LoginButton } from "../components";
+import React from 'react'
+import { LoginButton } from '../components'
 
 export default function LandingPage() {
   return (
-    <div className="landing-page-root"> 
+    <div className="landing-page-root">
+      <div className="logo-area">
+        Doc Tracer logo here!
+        <div>Let's get started!</div>
+      </div>
+
       <LoginButton />
     </div>
-  );
+  )
 }
