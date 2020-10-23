b9e112f17ffe58df5c0f5ae0d5ba3548598fb377
thc973 <thc973@usask.ca>
2020-10-23 12:03:31 -0600

    added right panel to home
    added the right panel to home as per Ricardo's request

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,15 @@ import React from 'react'
 export default function Home() {
   return (
   <div className="home-root">
+      <div className="lefter-container">
+          <div className="display_area">
+            
+          </div>
+          <div className="add_remove_buttons">
+              <button className="orange-button add_button">Add</button>
+              <button className="orange-button remove_button">Remove</button>
+          </div>
+      </div>
     <div className="left-container">
         <div className="home-header">
             Doc Tracer Logo
