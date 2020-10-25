c8309600ddaf6b36fdd750a1ede165576a04df76
Ricardo Rheeder <rir954@usask.ca>
2020-10-25 15:43:25 -0600

    Added a DocumentPanel component to be rendered for each object in a list

---
+++ client/src/components/DocumentPanel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const DocumentPanel= ({documentTitle}) => {
+  return (
+    <div className="document-panel-component">
+      {documentTitle}
+    </div>
+  )
+}
+
