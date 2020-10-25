ccaddfa6f2aaf2796091a908f256b4db575703de
Faiz <faizqureshi111@gmail.com>
2020-10-25 14:18:24 -0600

    reformated api calls to go into redux/stores/

--- client/src/redux/stores/index.js
+++ client/src/redux/stores/index.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from 'redux'
 
-import common from './common';
+import common from './common'
 
 export default combineReducers({
-    common,
-});
+  common,
+  backend,
+})
