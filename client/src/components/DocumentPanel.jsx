91b6c0bc3f0c96ac646ff037434d2ab8251b83f7
Ricardo Rheeder <rir954@usask.ca>
2020-10-25 17:13:12 -0600

    Added test dropdown boxes with styling

--- client/src/components/DocumentPanel.jsx
+++ client/src/components/DocumentPanel.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import Dropdown from 'react-dropdown'
 
-export const DocumentPanel= ({documentTitle}) => {
+export const DocumentPanel = ({ documentTitle }) => {
+  const testListOfVersions = ['1.1.0', '1.2.0']
+  const defaultOption = testListOfVersions[0]
+
+  const _onSelect = (thing) => {
+    console.log(thing)
+  }
+
   return (
     <div className="document-panel-component">
-      {documentTitle}
+      <div className="document-panel-title">
+        <h2>{documentTitle}</h2>
+      </div>
+
+      <div className="document-panel-dropdown">
+        <Dropdown
+          options={testListOfVersions}
+          onChange={_onSelect}
+          value={defaultOption}
+          placeholder="Select an option"
+          className="dropdown-custom-wrapper"
+          controlClassName="dropdown-custom-control"
+          placeholderClassName="dropdown-custom-placeholder"
+          menuClassName="dropdown-custom-menu"
+          arrowClassName="dropdown-custom-arrow"
+        />
+      </div>
     </div>
   )
 }
-
