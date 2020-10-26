e38a90cf014ce3afae1c417011aa95798f6e1879
Faiz <faizqureshi111@gmail.com>
2020-10-25 18:17:46 -0600

    added functionality for checking if user exists on login, if the user exists the users info will be saved to the inital state, otherwise the user will be created and the info will be saved in the db and set to the inital state.

--- client/src/redux/stores/user/actions.js
+++ client/src/redux/stores/user/actions.js
@@ -5,64 +5,55 @@ import {
 } from "./actionTypes";
 
 const axios = require("axios").default;
-const url = "http://localhost:5000/";
+const url = "http://localhost:5000/users";
 
 // fetching user information
 export const fetchUserInfoStart = () => {
   return {
-    type: actionTypes.FETCH_USER_INFO_START,
+    type: FETCH_USER_INFO_START,
   };
 };
 
 export const fetchUserInfoSuccess = (info) => {
   return {
-    type: actionTypes.FETCH_USER_INFO_SUCCESS,
+    type: FETCH_USER_INFO_SUCCESS,
     payload: info,
   };
 };
 
 export const fetchUserInfoFailure = (addMessage) => {
   return {
-    type: actionTypes.FETCH_USER_INFO_FAILURE,
+    type: FETCH_USER_INFO_FAILURE,
     payload: addMessage,
   };
 };
 
 export const fetchUserInfoAsync = (user) => {
+  // slicing the user id
+  const oldId = user.sub;
+  const newID = oldId.slice(6, oldId.length);
+
   return (dispatch) => {
     dispatch(fetchUserInfoStart());
     axios
-      .get(`${url}/users/get/${user.sub}`)
-      .then((info) => dispatch(fetchUserInfoSuccess(info)))
+      .get(`${url}/get/${newID}`)
+      .then((info) => {
+        if (info.data) {
+          dispatch(fetchUserInfoSuccess(info));
+        } else if (info.data === null) {
+          // // adding user since user does not already exist
+          axios
+            .post(`${url}/create-user`, {
+              _id: newID,
+              username: user.nickname,
+              email: user.email,
+            })
+            .then((res) => dispatch(fetchUserInfoSuccess(res)))
+            .catch((err) => console.log(err));
+        }
+      })
       .catch((error) => {
-        // adding user since user does not already exist
-        axios
-          .post(`${url}/users/create-user`, {
-            userID: user.sub,
-            username: user.nickname,
-            email: user.email,
-          })
-          .then((res) => dispatch(fetchUserInfoSuccess(res)))
-          .catch((err) => console.log("failed to create user."));
+        console.log("Error: " + error);
       });
   };
 };
-
-// // user creation
-// export const userCreationStart = () => {
-//   return {
-//     type: actionTypes.USER_CREATION_START,
-//   };
-// };
-
-// export const userCreationSuccess = () => {
-//   return {
-//     type: actionTypes.USER_CREATION_SUCCESS,
-//   };
-// };
-
-// export const userCreationFailure = () => {
-//   return {
-//     type: actionTypes.USER_CREATION_FAILURE,
-//   };
-// };
