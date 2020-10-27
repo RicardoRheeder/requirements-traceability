c902100887f1c8070d8f8350d7feef2048a18dc6
Faiz <faizqureshi111@gmail.com>
2020-10-26 19:11:26 -0600

    fixed typo

--- client/src/redux/stores/user/actions.js
+++ client/src/redux/stores/user/actions.js
@@ -2,46 +2,47 @@ import {
   FETCH_USER_INFO_START,
   FETCH_USER_INFO_SUCCESS,
   FETCH_USER_INFO_FAILURE,
-} from "./actionTypes";
+} from './actionTypes'
 
-const axios = require("axios").default;
-const url = "http://localhost:5000/users";
+const axios = require('axios').default
+const url = 'http://localhost:5000/users'
 
 // fetching user information
 export const fetchUserInfoStart = () => {
   return {
     type: FETCH_USER_INFO_START,
-  };
-};
+  }
+}
 
 export const fetchUserInfoSuccess = (info) => {
   return {
     type: FETCH_USER_INFO_SUCCESS,
     payload: info,
-  };
-};
+  }
+}
 
 export const fetchUserInfoFailure = (addMessage) => {
   return {
     type: FETCH_USER_INFO_FAILURE,
     payload: addMessage,
-  };
-};
+  }
+}
 
 export const fetchUserInfoAsync = (user) => {
-  // slicing the user id
-  const oldId = user.sub;
-  const newID = oldId.slice(6, oldId.length);
+  // slicing the user id to remove "auth0|"
+  const oldId = user.sub
+  const newID = oldId.slice(6, oldId.length)
 
   return (dispatch) => {
-    dispatch(fetchUserInfoStart());
+    dispatch(fetchUserInfoStart())
+    // getting the user if they exist otherwise creating a user
     axios
       .get(`${url}/get/${newID}`)
       .then((info) => {
         if (info.data) {
-          dispatch(fetchUserInfoSuccess(info));
+          dispatch(fetchUserInfoSuccess(info.data))
         } else if (info.data === null) {
-          // // adding user since user does not already exist
+          // adding user since user does not already exist
           axios
             .post(`${url}/create-user`, {
               _id: newID,
@@ -49,11 +50,11 @@ export const fetchUserInfoAsync = (user) => {
               email: user.email,
             })
             .then((res) => dispatch(fetchUserInfoSuccess(res)))
-            .catch((err) => console.log(err));
+            .catch((err) => console.log(err))
         }
       })
       .catch((error) => {
-        console.log("Error: " + error);
-      });
-  };
-};
+        console.log('Error: ' + error)
+      })
+  }
+}
