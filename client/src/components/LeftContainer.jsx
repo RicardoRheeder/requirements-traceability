6954c0440b8cc8fdfedbbc2083da107fab07f44f
Ricardo Rheeder <rir954@usask.ca>
2020-10-27 20:17:33 -0600

    Added ability to open modal using add and remove buttons

--- client/src/components/LeftContainer.jsx
+++ client/src/components/LeftContainer.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setModalObject } from '../redux/stores/common/actions'
 
 import { DocumentPanel } from './'
 
 export default function LeftContainer() {
+  const dispatch = useDispatch()
+
   let MockList = [
     'CMPT371ReqDoc',
     'Test Document',
@@ -24,13 +28,31 @@ export default function LeftContainer() {
     })
   }
 
+  const addDocumentButton = () => {
+    dispatch(setModalObject({ visible: true, mode: 0 }))
+  }
+
+  const removeDocumentButton = () => {
+    dispatch(setModalObject({ visible: true, mode: 1 }))
+  }
+
   return (
     <div className="left-container-root">
       {/* search bar here */}
       <div className="display-area">{RenderDocumentPanels(MockList)}</div>
       <div className="add-remove-buttons">
-        <button className="orange-button add-button">Add</button>
-        <button className="orange-button remove-button">Remove</button>
+        <button
+          className="orange-button add-button"
+          onClick={addDocumentButton}
+        >
+          Add
+        </button>
+        <button
+          className="orange-button remove-button"
+          onClick={removeDocumentButton}
+        >
+          Remove
+        </button>
       </div>
     </div>
   )
