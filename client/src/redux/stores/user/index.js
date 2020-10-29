0ef70533fb5e152b045493672d6e7fa34c7fff3f
Faiz <faizqureshi111@gmail.com>
2020-10-29 14:41:51 -0600

    updated state to include the error message on failure.

--- client/src/redux/stores/user/index.js
+++ client/src/redux/stores/user/index.js
@@ -2,18 +2,18 @@ import {
   FETCH_USER_INFO_START,
   FETCH_USER_INFO_SUCCESS,
   FETCH_USER_INFO_FAILURE,
-} from "./actionTypes";
-import initialState from "./initialState";
+} from './actionTypes'
+import initialState from './initialState'
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case FETCH_USER_INFO_START:
-      return { ...state, isFetching: true };
+      return { ...state, isFetching: true }
     case FETCH_USER_INFO_SUCCESS:
-      return { ...state, info: action.payload };
+      return { ...state, info: action.payload }
     case FETCH_USER_INFO_FAILURE:
-      return { ...state, isFetching: false };
+      return { ...state, isFetching: false, errorMessage: action.payload }
     default:
-      return state;
+      return state
   }
-};
+}
