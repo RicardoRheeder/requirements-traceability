237d8404a0ca15bb81efdc22e44a97635030078b
Faiz <faizqureshi111@gmail.com>
2020-10-29 18:14:52 -0600

    changed the async call to use react hooks

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
-import { connect, useDispatch } from 'react-redux'
+import { connect, useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { fetchUserInfoAsync } from '../redux/stores/user/actions'
 
-function Home({ fetchUserInfoAsync, isFetching, info, errorMessage }) {
+function Home() {
   // getting the current user who is signed in
   const { user } = useAuth0()
+  const dispatch = useDispatch()
+
+  // getting info and errorMessage from initial state
+  const userInfo = useSelector((state) => state.user.info)
+  const errorMessage = useSelector((state) => state.user.errorMessage)
 
   // use effect to fetch the user info when the component mounts
   useEffect(() => {
     if (user) {
-      fetchUserInfoAsync(user)
+      // dispatching async call with the user as a parameter
+      dispatch(fetchUserInfoAsync(user))
     }
   }, [fetchUserInfoAsync])
 
-  // console.log(info)
+  console.log(user)
+  console.log(userInfo)
+  console.log(errorMessage)
 
   return (
     <div className="home-root">
@@ -35,13 +43,4 @@ function Home({ fetchUserInfoAsync, isFetching, info, errorMessage }) {
   )
 }
 
-const mapStateToProps = (state) => ({
-  isFetching: state.user.isFetching,
-  info: state.user.info,
-  errorMessage: state.user.errorMessage,
-})
-const mapDispatchToProps = (dispatch) => ({
-  fetchUserInfoAsync: (user) => dispatch(fetchUserInfoAsync(user)),
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default Home
