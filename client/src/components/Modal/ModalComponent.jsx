3d2125f7f0a6e1cf85b8d9e1bdc846ac50db031f
Ricardo Rheeder <rir954@usask.ca>
2020-10-30 13:17:46 -0600

    Adjusted width and height of modal and edited contents of invite user modal

--- client/src/components/Modal/ModalComponent.jsx
+++ client/src/components/Modal/ModalComponent.jsx
@@ -13,6 +13,8 @@ const customStyles = {
     bottom: 'auto',
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
+    width: '50%',
+    height: '50%'
   },
 }
 
--- client/src/components/Modal/Modal_InviteUser.jsx
+++ client/src/components/Modal/Modal_InviteUser.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 export default function Modal_InviteUser() {
   return (
     <div>
-      <h2>Invite user to document</h2>
-      <div>Please enter the email address of the user to invite.</div>
-      <label for="emails">Enter email addresses:</label>
+      <h2>{'Invite user(s) to document'}</h2>
+      <div>
+        {'Please enter the email address(es) of the user(s) to invite.'}
+      </div>
+      <div>
+        {'When entering multiple email addresses, use commas to separate.'}
+      </div>
       <form>
         <input type="email" id="emails" name="emails" multiple />
         <button>Submit</button>
