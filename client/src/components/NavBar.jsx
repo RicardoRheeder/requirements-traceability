0130d6c9f1cd2970b524efeb6c03dae95a1b9fcf
Ricardo Rheeder <rir954@usask.ca>
2020-10-30 14:29:30 -0600

    Reformatting all styling for navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -18,63 +18,73 @@ export default function NavBar() {
   }, [])
 
   return (
-    <div className="navbar-root">
-      <nav
-        className="navbar navbar-dark navbar-expand-lg fixed-top bg-dark mainNav"
-        id="mainNav"
-      >
-        <div className="container">
-          <a className="navbar-brand">
-            <img
-              className="nav-icon"
-              src="/assets/images/Doc_Tracer_Icon_1_gradient.png"
-            ></img>
-          </a>
-          <div>
-            <ul className="navbar-nav text-uppercase">
-              {isAuthenticated ? (
-                <li className="nav-item">
-                  <NavLink to={'/'} id="NavToHome">
-                    <div className="nav-link">Home</div>
-                  </NavLink>
-                </li>
-              ) : (
-                <></>
-              )}
+    <div className="navbar-root topnav fixed-top">
+      <img
+        className="nav-icon"
+        src="/assets/images/Doc_Tracer_Icon_1_gradient.png"
+      ></img>
 
-              {isAuthenticated ? (
-                <li className="nav-item">
-                  <NavLink to={'/editor'} id="NavToEditor">
-                    <div className="nav-link">Editor</div>
-                  </NavLink>
-                </li>
-              ) : (
-                <></>
-              )}
+      <div className="navbar-options-container">
+        <ul className="navbar-option text-uppercase">
+          {isAuthenticated ? (
+            <li className="nav-item">
+              <NavLink to={'/home'} id="NavToHome">
+                <div className="nav-link">Home</div>
+              </NavLink>
+            </li>
+          ) : (
+            <></>
+          )}
+          {isAuthenticated ? (
+            <li className="nav-item">
+              <NavLink to={'/editor'} id="NavToEditor">
+                <div className="nav-link">Editor</div>
+              </NavLink>
+            </li>
+          ) : (
+            <></>
+          )}
 
-              <li className="nav-item">
-                <a className="nav-link">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Team</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link">Contact</a>
-              </li>
+          <li className="nav-item">
+            <NavLink to={'/about'} id="NavToAbout">
+              <div className="nav-link">About</div>
+            </NavLink>
+          </li>
 
-              {isAuthenticated ? (
-                <li className="nav-item">
-                  <a className="nav-link">
-                    <LogoutButton />
-                  </a>
-                </li>
-              ) : (
-                <></>
-              )}
-            </ul>
-          </div>
-        </div>
-      </nav>
+          {isAuthenticated ? (
+            <li className="nav-item">
+              <a className="nav-link">
+                <LogoutButton />
+              </a>
+            </li>
+          ) : (
+            <></>
+          )}
+        </ul>
+      </div>
     </div>
+    // <div className="navbar-root topnav fixed-top">
+    //     <div className="container">
+    //       <a className="navbar-brand">
+    //
+    //       </a>
+    //       <div className="navbar-options-container">
+    //         <ul className="navbar-nav text-uppercase">
+    //
+
+    //           <li className="nav-item">
+    //             <a className="nav-link">About</a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link">Team</a>
+    //           </li>
+    //           <li className="nav-item">
+    //             <a className="nav-link">Contact</a>
+    //           </li>
+
+    //         </ul>
+    //       </div>
+    //     </div>
+    //   </div>
   )
 }
