3d9b2f70cec6f5d30e28e2a37755d66054f2dbbf
Faiz <faizqureshi111@gmail.com>
2020-10-31 14:41:49 -0600

    coppied @shh738 documents reducer

---
+++ client/src/redux/stores/document/actions.js
@@ -0,0 +1,87 @@
+import {
+    CREATE_DOC_START,
+    CREATE_DOC_FAILURE,
+    CREATE_DOC_SUCCESS,
+    DELETE_DOC_START,
+    DELETE_DOC_FAILURE,
+    DELETE_DOC_SUCCESS,
+} from './actionTypes';
+
+const axios = require('axios').default;
+
+const url = "http://localhost:5000/documents";
+
+/**
+ * Function to start
+ */
+export const createDocStart = ()=>{
+    return {
+        type: CREATE_DOC_START
+    }
+} 
+
+export const deleteDocStart = ()=>{
+    return{
+        type: DELETE_DOC_START
+    }
+}
+
+export const createDocSuccess = (doc)=>{
+    return {
+        type: CREATE_DOC_SUCCESS,
+        data: doc
+    }
+}
+
+export const deleteDocSuccess = (docs)=>{
+    return{
+        type: DELETE_DOC_SUCCESS,
+        data: docs
+    }
+}
+
+export const createDocFailure = (err)=>{
+    return {
+        type: CREATE_DOC_SUCCESS,
+        data: err
+    }
+}
+
+export const deleteDocFailure = (err)=>{
+    return{
+        type: DELETE_DOC_FAILURE,
+        data: err
+    }
+}
+
+
+export const createDocAsync = (doc)=>{
+    return (dispatch)=>{
+        dispatch(createDocStart())
+        axios.post(`${url}/create-document`, {title: doc.title, admin: doc.admin})
+        .then((doc)=>{
+            console.log(doc)
+            dispatch(createDocSuccess(doc))
+        })
+        .catch((err)=>{
+            console.log(err)
+            dispatch(createDocFailure(err))
+        })
+    }
+}
+
+export const deleteDocAsync = (doc)=>{
+    return(dispatch)=>{
+        dispatch(deleteDocStart())
+        axios.delete(`${url}/delete/${doc.id}`, {user: doc.userID})
+        .then((newList)=>{
+            dispatch(createDocSuccess(newList))
+        })
+        .catch((err)=>{
+            dispatch(createDocFailure(err))
+        })
+    }
+}
+
+
+
