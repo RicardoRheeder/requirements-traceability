ffa60c12fbd6bbc0d556cca87412d124ed3210d8
Faiz <faizqureshi111@gmail.com>
2020-10-31 14:44:11 -0600

    fixed the isfetching state

--- client/src/redux/stores/document/index.js
+++ client/src/redux/stores/document/index.js
@@ -1,29 +1,29 @@
 import {
-    CREATE_DOC_START,
-    CREATE_DOC_FAILURE,
-    CREATE_DOC_SUCCESS,
-    DELETE_DOC_START,
-    DELETE_DOC_FAILURE,
-    DELETE_DOC_SUCCESS
-} from './actionTypes';
+  CREATE_DOC_START,
+  CREATE_DOC_FAILURE,
+  CREATE_DOC_SUCCESS,
+  DELETE_DOC_START,
+  DELETE_DOC_FAILURE,
+  DELETE_DOC_SUCCESS,
+} from './actionTypes'
 
-import initialState from './initialState';
+import initialState from './initialState'
 
 export default (state = initialState, action) => {
-    switch (action.type) {
-        case CREATE_DOC_START:
-            return { ...state, isFetching: action.data };
-        case CREATE_DOC_FAILURE:
-            return { ...state, isFetching: action.data, error: action.data };
-        case CREATE_DOC_SUCCESS:
-            return { ...state, current_doc: action.data };
-        case DELETE_DOC_START:
-            return { ...state, isFetching: action.data };
-        case DELETE_DOC_FAILURE:
-            return { ...state, isFetching: action.data, error: action.data};
-        case DELETE_DOC_SUCCESS:
-                return { ...state, documents: action.data };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case CREATE_DOC_START:
+      return { ...state, isFetching: true }
+    case CREATE_DOC_FAILURE:
+      return { ...state, isFetching: false, error: action.data }
+    case CREATE_DOC_SUCCESS:
+      return { ...state, isFetching: false, current_doc: action.data }
+    case DELETE_DOC_START:
+      return { ...state, isFetching: true }
+    case DELETE_DOC_FAILURE:
+      return { ...state, isFetching: false, error: action.data }
+    case DELETE_DOC_SUCCESS:
+      return { ...state, isFetching: false, documents: action.data }
+    default:
+      return state
+  }
 }
