3bc5f9520e6036d9762d9930ed3116e62e2393f8
Faiz <faizqureshi111@gmail.com>
2020-10-31 12:27:50 -0600

    added route to get user documents with user email

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -37,7 +37,6 @@ router.route("/get/:id").get((req, res) => {
 
 // getting a specific user using their email
 router.route("/get-by-email/:email").get((req, res) => {
-  console.log(req.params.email);
   User.findOne({ email: req.params.email })
     .then((user) => res.json(user))
     .catch((err) => res.status(400).json("Error: " + err));
@@ -52,6 +51,14 @@ router.route("/get/documents/:id").get((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+// getting a users documents with email
+router.route("/get/documents-with-email/:email").get((req, res) => {
+  User.findOne({ email: req.params.email })
+    .populate("documents")
+    .exec()
+    .then((user) => res.json(user.documents))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
 // Update Routes*****************************************
 
 // updating a specific user
