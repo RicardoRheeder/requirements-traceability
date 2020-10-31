106be64bcc085d02e5efdc86d4d15dbcbd22a002
Mitchell <miy456@mail.usask.ca>
2020-10-31 15:05:58 -0600

    Received updated front end code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,36 +1,9 @@
 import React from 'react'
-<<<<<<< HEAD
-import { connect, useDispatch, useSelector } from 'react-redux'
-=======
 import { connect, useDispatch } from 'react-redux'
->>>>>>> rir954-shh738-faq588/test/testing-async-calls-from-frontend
 import { useEffect } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { fetchUserInfoAsync } from '../redux/stores/user/actions'
 import { LeftContainer } from '../components'
-<<<<<<< HEAD
-
-function Home() {
-  // getting the current user who is signed in
-  const { user } = useAuth0()
-  const dispatch = useDispatch()
-
-  // getting info and errorMessage from initial state
-  const userInfo = useSelector((state) => state.user.info)
-  const errorMessage = useSelector((state) => state.user.errorMessage)
-
-  // use effect to fetch the user info when the component mounts
-  useEffect(() => {
-    if (user) {
-      // dispatching async call with the user as a parameter
-      dispatch(fetchUserInfoAsync(user))
-    }
-  }, [fetchUserInfoAsync])
-
-  console.log(user)
-  console.log(userInfo)
-  console.log(errorMessage)
-=======
 
 function Home({ fetchUserInfoAsync, isFetching, info, errorMessage }) {
   // console.log(isFetching);
@@ -43,7 +16,6 @@ function Home({ fetchUserInfoAsync, isFetching, info, errorMessage }) {
   }, [fetchUserInfoAsync])
 
   console.log(info)
->>>>>>> rir954-shh738-faq588/test/testing-async-calls-from-frontend
 
   return (
     <div className="home-root">
@@ -51,13 +23,7 @@ function Home({ fetchUserInfoAsync, isFetching, info, errorMessage }) {
         <LeftContainer />
       </div>
       <div className="center-container">
-<<<<<<< HEAD
-        <div className="home-header">
-          <img className="home-logo-banner" src="/assets/images/Doc_Tracer_Logo_2.png"></img>
-        </div>
-=======
         <div className="home-header">Doc Tracer Logo</div>
->>>>>>> rir954-shh738-faq588/test/testing-async-calls-from-frontend
         <div className="home-subheader">Recent Documents</div>
       </div>
       <div className="right-container">Notifications</div>
@@ -65,9 +31,6 @@ function Home({ fetchUserInfoAsync, isFetching, info, errorMessage }) {
   )
 }
 
-<<<<<<< HEAD
-export default Home
-=======
 const mapStateToProps = (state) => ({
   isFetching: state.user.isFetching,
   info: state.user.info,
@@ -78,4 +41,3 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
->>>>>>> rir954-shh738-faq588/test/testing-async-calls-from-frontend
