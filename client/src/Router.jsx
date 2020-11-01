0252fe5d21b25168e8d9b57ba1085925ab06b185
Ricardo Rheeder <rir954@usask.ca>
2020-10-31 23:20:54 -0600

    Created a loading screen for when logging in

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -11,7 +11,7 @@ import { useSelector } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react'
 
 import { css } from '@emotion/core'
-import PropagateLoader from 'react-spinners/PropagateLoader'
+import PacmanLoader from 'react-spinners/PacmanLoader'
 
 export default function Router() {
   const { isAuthenticated, isLoading } = useAuth0()
@@ -21,7 +21,27 @@ export default function Router() {
     margin: auto auto;
     border-color: red;
     background-color: #000000 !important;
+    position: relative;
+    top: 45% !important;
+    right: auto !important;
+    bottom: auto !important;
+    height: 50%;
   `
+
+  const loaderObject = () => {
+    return isLoading ? (
+      <div className={isLoading ? 'loader-container' : ''}>
+        <PacmanLoader
+          css={override}
+          size={60}
+          color={'#f96302'}
+          loading={isLoading}
+        />
+      </div>
+    ) : (
+      <></>
+    )
+  }
 
   return (
     <div className="app-root">
@@ -29,29 +49,17 @@ export default function Router() {
         <Switch>
           {!isAuthenticated ? (
             <Route path="*">
+              {loaderObject()}
               <NavBar />
               <LandingPage />
             </Route>
           ) : (
             <>
-              {true ? (
-                <>
-                  <div className="loader-container">
-                    <PropagateLoader
-                      css={override}
-                      size={150}
-                      color={'#f96302'}
-                      loading={true}
-                    />
-                  </div>
-                  <ModalComponent />
-                  <NavBar />
-                  <Route exact={true} path="/" children={<Home />} />
-                  <Route exact={true} path="/editor" children={<Editor />} />
-                </>
-              ) : (
-                <></>
-              )}
+              {loaderObject()}
+              <ModalComponent />
+              <NavBar />
+              <Route exact={true} path="/" children={<Home />} />
+              <Route exact={true} path="/editor" children={<Editor />} />
             </>
           )}
         </Switch>
