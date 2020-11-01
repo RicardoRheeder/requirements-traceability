db89dfe76989fbf3c8fe544a04d9d2374c4b1ae7
shh738 <shh738@usask.ca>
2020-11-01 01:06:37 -0600

    Fixed merge conflict with master

--- client/src/redux/stores/document/actionTypes.js
+++ client/src/redux/stores/document/actionTypes.js
@@ -15,11 +15,8 @@ export const FETCH_USER_DOCS_START = 'FETCH_USER_DOCS_START'
 export const FETCH_USER_DOCS_SUCCESS = 'FETCH_USER_DOCS_SUCCESS'
 export const FETCH_USER_DOCS_FAILURE = 'FETCH_USER_DOCS_FAILURE'
 
-<<<<<<< HEAD
 export const UPDATE_CURRENT_DOCUMENT = 'UPDATE_CURRENT_DOCUMENT'
-=======
 // actions for adding a user to a document
 export const ADD_USER_TO_DOC_START = 'ADD_USER_TO_DOC_START';
 export const ADD_USER_TO_DOC_FAILURE = 'ADD_USER_TO_DOC_FAILURE';
 export const ADD_USER_TO_DOC_SUCCESS = 'ADD_USER_TO_DOC_SUCCESS';
->>>>>>> 4114413... Finished implentation of backend route to add user to a document and setup the store and actions for the front end
