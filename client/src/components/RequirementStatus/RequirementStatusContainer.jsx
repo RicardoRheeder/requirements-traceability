6b7a7d06fd01969ec0bb6e851fabb692d2a004cd
Mitchell <miy456@mail.usask.ca>
2020-11-23 14:35:30 -0600

    Added support for displaying statuses not included in the presets

--- client/src/components/RequirementStatus/RequirementStatusContainer.jsx
+++ client/src/components/RequirementStatus/RequirementStatusContainer.jsx
@@ -3,13 +3,22 @@ import React from 'react'
 export default function RequirementStatusContainer({listOfStatuses}) {
     const renderStatuses = (statusList) => {
         console.log(statusList);
+        var presets = ["satisfied","unsatisfied","WIP","review"];
         return statusList.map((item, i) => {
             console.log(item);
+            if(presets.includes(item)){
             return (
             <span key = {i} className={item}>
                 {item}
             </span>
-            )
+            )}
+            else{
+                return (
+                    <span key = {i} className="created-status">
+                        {item}
+                    </span>
+                    )
+            }
         });
     }
     return (
