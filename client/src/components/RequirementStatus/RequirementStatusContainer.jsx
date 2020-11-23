740fc57f9b56c60cf5895263311e9d9b879e68f8
Mitchell <miy456@mail.usask.ca>
2020-11-23 15:13:43 -0600

    Added CSS options for different colours of user created statuses and altered how a user created status is stored in the initialState

--- client/src/components/RequirementStatus/RequirementStatusContainer.jsx
+++ client/src/components/RequirementStatus/RequirementStatusContainer.jsx
@@ -13,11 +13,51 @@ export default function RequirementStatusContainer({listOfStatuses}) {
             </span>
             )}
             else{
-                return (
-                    <span key = {i} className="created-status">
-                        {item}
-                    </span>
-                    )
+                switch(item[1]){
+                    case "blue":
+                        return (
+                            <span key = {i} className="created-status-blue">
+                                {item[0]}
+                            </span>
+                            )
+                    case "red":
+                        return (
+                            <span key = {i} className="created-status-red">
+                                {item[0]}
+                            </span>
+                            )
+                    case "green":
+                        return (
+                            <span key = {i} className="created-status-green">
+                                {item[0]}
+                            </span>
+                            )
+                    case "yellow":
+                        return (
+                            <span key = {i} className="created-status-yellow">
+                                {item[0]}
+                            </span>
+                            )
+                    case "orange":
+                        return (
+                            <span key = {i} className="created-status-orange">
+                                {item[0]}
+                            </span>
+                            )
+                    case "grey":
+                        return (
+                            <span key = {i} className="created-status-grey">
+                                {item[0]}
+                            </span>
+                            )
+                    default:
+                        return (
+                            <span key = {i} className="created-status-grey">
+                                {item[0]}
+                            </span>
+                            )
+                }
+                
             }
         });
     }
