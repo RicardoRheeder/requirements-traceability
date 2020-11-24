284035972d786ca4c6a85402cd078a19167793b5
jrk057 <jrk057@usask.ca>
2020-11-24 04:59:23 -0600

    added functional route testing

--- backend/test/document-test.js
+++ backend/test/document-test.js
@@ -1,16 +1,22 @@
 process.env.NODE_ENV = 'test';
 
+
+let mongoose = require('mongoose')
 const stub = require('sinon')
 let Document = require('../models/document.model');
+const User = require('../models/user.model')
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 const routes = require('../routes/document')
 const document = require('../models/document.model')
 let server = require('../server');
-const { connection } = require('mongoose');
+const { connection, Schema } = require('mongoose');
+const { test } = require('mocha');
 let should = chai.should();
-
+let id = require('mongoose').Types.ObjectId();
 chai.use(chaiHttp);
+
+
 
 describe('document routes', function() {
     beforeEach((done) => {
@@ -23,15 +29,14 @@ describe('document routes', function() {
 
     describe('/', () => {
         it('should get all the docs', (done) =>{
-            chai.request(server).get('/').end((err,res) => {
+            chai.request(server).get('/documents').end((err,res) => {
                 res.should.have.status(200);
-                res.body.should.be.empty;
-                res.body.should.be.a('object');
-                
+                res.body.response.should.be.a('array'); 
+                res.body.response.should.be.empty
+                done();
             })
-            done();
-        })
-        
+            
+        })   
     })
     
 })
